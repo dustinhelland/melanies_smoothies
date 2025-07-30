94a7b96233678233546830cd0e87b4cbca3645d3
dustinhelland <32628750+dustinhelland@users.noreply.github.com>
2025-07-30 10:29:11 -0500

    Update streamlit_app.py
    new format for nutr info

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
@@ -46,7 +47,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
-
-import requests
-smoothiefroot_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
