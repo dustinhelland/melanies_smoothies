626037494b0a71f27ecb23dbb4d1d65528d31095
dustinhelland <32628750+dustinhelland@users.noreply.github.com>
2025-07-30 10:23:39 -0500

    Update streamlit_app.py
    fixed type error in last update

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,4 @@ if ingredients_list:
 import requests
 smoothiefroot_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), user_container_width=True)
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
